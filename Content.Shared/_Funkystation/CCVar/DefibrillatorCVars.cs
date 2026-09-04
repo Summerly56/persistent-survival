37d3efdcde62d8fd3367629dd5fc1427fadbbfff
DenconstructsWalls <jhncosta2019@gmail.com>
2026-09-04 20:08:18 -0300

    Defib CVar changes
    40% to 70% defib chance
    5 to 1 adrenaline cost

--- Content.Shared/_Funkystation/CCVar/DefibrillatorCVars.cs
+++ Content.Shared/_Funkystation/CCVar/DefibrillatorCVars.cs
@@ -9,11 +9,11 @@ public sealed class DefibrillatorCVars
     /// chance for a zap to resuscitate a dead person
     /// </summary>
     public static readonly CVarDef<float> ReviveChance =
-        CVarDef.Create("funkystation.defib.revive_chance", 0.4f, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("funkystation.defib.revive_chance", 0.7f, CVar.SERVER | CVar.REPLICATED);
 
     /// <summary>
     /// amount of adrenaline reagent consumed per zap
     /// </summary>
     public static readonly CVarDef<float> AdrenalineCost =
-        CVarDef.Create("funkystation.defib.adrenaline_cost", 5f, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("funkystation.defib.adrenaline_cost", 1f, CVar.SERVER | CVar.REPLICATED);
 }
